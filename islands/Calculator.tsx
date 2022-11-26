73a391ac549f4b816bc3bcd36bd325bb296acabb
justin kempton <justin@talentsky.com>
2022-11-25 16:14:01 -0800

    seeing if this speeds up

--- islands/Calculator.tsx
+++ islands/Calculator.tsx
@@ -1,53 +1,55 @@
 import { Head } from "$fresh/runtime.ts";
 import { CurrentStack, CurrentValue } from '../data/Calculations.ts';
 
-const Action = ({ className, children }) => {
+const Action = ({ className, children, onMouseDown }) => {
   return (
     <button
-      {...{ className, children }}
-      onMouseDown={e => {
-
-        let thing = e.currentTarget.innerText
-
-        switch (thing) {
-
-          case "C": {
-            CurrentStack.value = []
-            return
-          }
-
-          case "⌫": {
-            let most = CurrentStack.value.slice(0, CurrentStack.value.length - 1)
-            CurrentStack.value = most
-            return
-          }
-
-          case "⏾":
-            return
-
-          case "=": {
-            debugger
-            return
-          }
-
-        }
-
-        CurrentStack.value = [...CurrentStack.value, thing]
-
-        switch (thing) {
-          case "÷":
-          case "-":
-          case "+":
-          case "×":
-            return CurrentValue.value = CurrentStack.value
-        }
-
-      }}
+      {...{ className, children, onMouseDown }}
     />
   )
 }
 
 export default () => {
+
+  const onMouseDown = e => {
+
+    let thing = e.currentTarget.innerText
+
+    switch (thing) {
+
+      case "C": {
+        CurrentStack.value = []
+        return
+      }
+
+      case "⌫": {
+        let most = CurrentStack.value.slice(0, CurrentStack.value.length - 1)
+        CurrentStack.value = most
+        return
+      }
+
+      case "⏾":
+        return
+
+      case "=": {
+        debugger
+        return
+      }
+
+    }
+
+    CurrentStack.value = [...CurrentStack.value, thing]
+
+    switch (thing) {
+      case "÷":
+      case "-":
+      case "+":
+      case "×":
+        return CurrentValue.value = CurrentStack.value
+    }
+
+  }
+
   return (
     <div class="calculator">
       <Head>
@@ -72,25 +74,25 @@ export default () => {
         <div class="calculator-grid">
           <span class="i">{CurrentValue.value.join("") || 0}</span>
           <span class="i i2">{CurrentStack.value.join("") || 0}</span>
-          <Action className="g">C</Action>
-          <Action className="g">⌫</Action>
-          <Action className="g">⏾</Action>
-          <Action className="a">÷</Action>
-          <Action className="n">7</Action>
-          <Action className="n">8</Action>
-          <Action className="n">9</Action>
-          <Action className="a">+</Action>
-          <Action className="n">4</Action>
-          <Action className="n">5</Action>
-          <Action className="n">6</Action>
-          <Action className="a">-</Action>
-          <Action className="n">1</Action>
-          <Action className="n">2</Action>
-          <Action className="n">3</Action>
-          <Action className="a">×</Action>
-          <Action className="n double">0</Action>
-          <Action className="n">.</Action>
-          <Action className="a">=</Action>
+          <Action onMouseDown={onMouseDown} className="g">C</Action>
+          <Action onMouseDown={onMouseDown} className="g">⌫</Action>
+          <Action onMouseDown={onMouseDown} className="g">⏾</Action>
+          <Action onMouseDown={onMouseDown} className="a">÷</Action>
+          <Action onMouseDown={onMouseDown} className="n">7</Action>
+          <Action onMouseDown={onMouseDown} className="n">8</Action>
+          <Action onMouseDown={onMouseDown} className="n">9</Action>
+          <Action onMouseDown={onMouseDown} className="a">+</Action>
+          <Action onMouseDown={onMouseDown} className="n">4</Action>
+          <Action onMouseDown={onMouseDown} className="n">5</Action>
+          <Action onMouseDown={onMouseDown} className="n">6</Action>
+          <Action onMouseDown={onMouseDown} className="a">-</Action>
+          <Action onMouseDown={onMouseDown} className="n">1</Action>
+          <Action onMouseDown={onMouseDown} className="n">2</Action>
+          <Action onMouseDown={onMouseDown} className="n">3</Action>
+          <Action onMouseDown={onMouseDown} className="a">×</Action>
+          <Action onMouseDown={onMouseDown} className="n double">0</Action>
+          <Action onMouseDown={onMouseDown} className="n">.</Action>
+          <Action onMouseDown={onMouseDown} className="a">=</Action>
         </div>
       </div>
     </div>
