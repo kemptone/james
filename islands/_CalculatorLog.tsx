3a19ad8334ab823ff721635003ccf2a4c46b47f2
justin kempton <justin@talentsky.com>
2022-12-10 22:47:37 -0800

    WARNING MESSAGE

--- islands/_CalculatorLog.tsx
+++ islands/_CalculatorLog.tsx
@@ -6,6 +6,12 @@ export default e => {
   return (
     <>
       <div class="log">
+        <div>
+          **** WARNING *** demo only, CALCULATOR IS NOT ACCURATE WITH DECIMAL
+        </div>
+        <div>
+          **** PROBABLY OTHER BUGS AS WELL **** DO NOT USE FOR REAL NUMBERS
+        </div>
         {AllStacks.value.map((item, index) => {
           return (
             <div key={index}>
