d184c8fb3d7860e5bbebebb7e464b70d2b8fedcd
justin kempton <justin@talentsky.com>
2022-12-17 22:30:55 -0800

    adding inset stuff

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,12 +5,14 @@ export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
-        {/* <link rel="stylesheet" href="reset.css" /> */}
+        <link rel="manifest" href="manifest.json" />
         <link rel="stylesheet" href="menu.css" />
         <link rel="stylesheet" href="style.css" />
         <link rel="stylesheet" href="dialog.css" />
         <link rel="stylesheet" href="fonts.css" />
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <Component />
     </>
