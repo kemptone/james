d127c81e198728cb2de073dfe870f7016e950cc9
justin kempton <justin@talentsky.com>
2022-12-24 12:12:31 -0800

    just cleanup

--- static/loadserviceworker.js
+++ static/loadserviceworker.js
@@ -1,16 +1,9 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceworker.js').then(registration => {
-      console.log('Service worker registered: ', registration);
+    navigator.serviceWorker.register('/serviceworker.js').then( registration => {
+      console.log('Service worker registered: ', registration)
     }).catch(error => {
-      console.log('Service worker registration failed: ', error);
-    });
-  });
+      console.log('Service worker registration failed: ', error)
+    })
+  })
 }
-
-
-// This code creates a button that the user can click to reset the cache
-// const resetButton = document.getElementById('reset-button');
-// resetButton.addEventListener('click', () => {
-//   caches.delete('my-cache');
-// });
--- static/serviceworker.js
+++ static/serviceworker.js
@@ -1,24 +1,26 @@
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url)
 
+  // these files will not be cached at all
   if (
     url.protocol === 'chrome-extension:'
     || url.pathname === "/_frsh/alive"
     || url.pathname === "/_frsh/refresh.js"
   ) {
-    console.log(event.request.url)
-    event.respondWith(fetch(event.request));
-  } else {
-    // Otherwise, cache the response and return it
-    event.respondWith(
-      caches.open('my-cache').then(cache => {
-        return cache.match(event.request).then(response => {
-          return response || fetch(event.request).then(response => {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        });
+    event.respondWith(fetch(event.request))
+    return
+  } 
+  
+  // Otherwise, cache the response and return it
+  event.respondWith(
+    caches.open('my-cache').then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(response => {
+          cache.put(event.request, response.clone())
+          return response
+        })
       })
-    );
-  }
-});
+    })
+  )
+
+})
