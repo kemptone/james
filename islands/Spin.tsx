367235e8be37d9342dab8afb1e840461708df7ac
justin kempton <justin@talentsky.com>
2023-02-17 00:41:07 -0800

    more fun with fans

--- islands/Spin.tsx
+++ islands/Spin.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "preact/hooks";
 import Dialog from "../components/Dialog.tsx";
 
+const Fans = "Dumpy.png Fardo.png Lark.png Orange.png"
+  .split(" ");
+
 const OnAnimationIteration = (doThis: () => void) => (e: Event) => {
   doThis();
 };
@@ -9,6 +12,9 @@ export default (props: {}) => {
   const [state, setState] = useState("");
   const [totalTime, setTotalTime] = useState(10);
   const [totalRotations, setTotalRotations] = useState(6);
+  const [currentFan, setCurrentFan] = useState("Dumpy.png");
+  const [darkmode, setDarkmode] = useState(false);
+  const [darkmode2, setDarkmode2] = useState(false);
 
   useEffect(() => {
     document?.body?.style?.setProperty?.("--spintimer", totalTime + "s");
@@ -16,14 +22,24 @@ export default (props: {}) => {
       "--totalrotations",
       (360 * totalRotations) + "deg",
     );
+    if (darkmode) {
+      document?.body?.classList?.add("darkmode");
+    } else {
+      document?.body?.classList?.remove("darkmode");
+    }
+    if (darkmode2) {
+      document?.body?.classList?.add("darkmode2");
+    } else {
+      document?.body?.classList?.remove("darkmode2");
+    }
     setState("");
-  }, [totalTime, totalRotations]);
+  }, [totalTime, totalRotations, darkmode, darkmode2]);
 
   return (
     <>
       <div className={`spinner ${state}`}>
         <img
-          src="/img_489522.png"
+          src={`/spin/${currentFan}`}
           onTransitionEnd={(e) => {
             setState("return");
             setTimeout((e) => {
@@ -40,6 +56,27 @@ export default (props: {}) => {
         />
       </div>
       <div className="control">
+        <fieldset>
+          <legend>darkmode</legend>
+          <input
+            type="checkbox"
+            onChange={(e) => setDarkmode(e?.currentTarget?.checked)}
+          />
+          <input
+            type="checkbox"
+            onChange={(e) => setDarkmode2(e?.currentTarget?.checked)}
+          />
+        </fieldset>
+
+        <fieldset>
+          <legend>Fan</legend>
+          <select
+            onChange={(e) => setCurrentFan(e.currentTarget.value)}
+            defaultValue={currentFan}
+          >
+            {Fans.map((fan) => <option value={fan}>{fan}</option>)}
+          </select>
+        </fieldset>
         <fieldset>
           <legend>Time, in seconds</legend>
           <input
