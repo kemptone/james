3f8b6200d4a95e0d94308a478a8978fc3bcbfe25
justin kempton <justin@talentsky.com>
2023-02-19 13:45:56 -0800

    trying to fix context issue

--- islands/Spin.tsx
+++ islands/Spin.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "preact/hooks";
+// import spinsounds from "../sounds/spinsounds.tsx";
 import runSpinSound from "../sounds/spinsounds.tsx";
 import runSwooshSound from "../sounds/swooshsound.tsx";
 
@@ -17,6 +18,79 @@ export default (props: {}) => {
   const [currentFan, setCurrentFan] = useState("Dumpy.png");
   const [darkmode, setDarkmode] = useState(false);
   const [darkmode2, setDarkmode2] = useState(false);
+  const [context, setContext] = useState<AudioContext | undefined>();
+  const [sourceSwoosh, setSourceSwoosh] = useState<AudioBufferSourceNode>();
+  const [sourceMain, setSourceMain] = useState<AudioBufferSourceNode>();
+
+  function startSound() {
+    const context = new AudioContext();
+    setContext(context);
+
+    fetch("/spin/swoop_206.mp3")
+      .then((response) => response.arrayBuffer())
+      .then((buffer) => context.decodeAudioData(buffer))
+      .then((audioBuffer) => {
+        const sourceSwoosh = runSpinSound(
+          totalTime,
+          totalRotations,
+          context,
+          audioBuffer,
+        );
+        setSourceSwoosh(sourceSwoosh);
+        sourceSwoosh.start();
+        // setSourceSwoosh(
+        //   runSpinSound(totalTime, totalRotations, context, audioBuffer),
+        // );
+      });
+    fetch("/spin/main_206.mp3")
+      .then((response) => response.arrayBuffer())
+      .then((buffer) => context.decodeAudioData(buffer))
+      .then((audioBuffer) => {
+        const sourceMain = runSwooshSound(
+          totalTime,
+          totalRotations,
+          context,
+          audioBuffer,
+        );
+        sourceMain.start();
+        setSourceMain(sourceMain);
+      });
+  }
+
+  function stopSound() {
+  }
+
+  useEffect(() => {
+    console.log("useEffect");
+    return () => {
+      // context?.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    // const context = new AudioContext();
+    // setContext(context);
+
+    // if (!context) {
+    //   debugger;
+    //   return;
+    // }
+
+    // fetch("/spin/swoop_206.mp3")
+    //   .then((response) => response.arrayBuffer())
+    //   .then((buffer) => context.decodeAudioData(buffer))
+    //   .then((audioBuffer) => {
+    //     setSourceSwoosh(
+    //       runSpinSound(totalTime, totalRotations, context, audioBuffer),
+    //     );
+    //   });
+    // fetch("/spin/main_206.mp3")
+    //   .then((response) => response.arrayBuffer())
+    //   .then((buffer) => context.decodeAudioData(buffer))
+    //   .then((audioBuffer) => {
+    //     runSwooshSound(totalTime, totalRotations, context, audioBuffer);
+    //   });
+  }, [totalTime, totalRotations, context]);
 
   useEffect(() => {
     if (darkmode) {
@@ -40,15 +114,6 @@ export default (props: {}) => {
     setState("");
   }, [totalTime, totalRotations]);
 
-  // useEffect(() => {
-  //   if (state !== "spin") {
-  //     return;
-  //   }
-
-  //   // runSpinSound(totalTime, totalRotations);
-  //   // runSwooshSound(totalTime, totalRotations);
-  // }, [state, totalTime, totalRotations]);
-
   return (
     <>
       <div className={`spinner ${state}`}>
@@ -56,18 +121,25 @@ export default (props: {}) => {
           src={`/spin/${currentFan}`}
           onTransitionEnd={(e) => {
             setState("return");
+            sourceSwoosh?.stop();
+            sourceMain?.stop();
+            context?.close();
+            // context?.close();
             setTimeout((e) => {
               setState("");
+              // setContext(new AudioContext());
             }, 100);
           }}
           onClick={(e) => {
+            // setContext(new AudioContext());
+
             if (state) {
               setState("");
             } else {
               setState("spin");
-              const context = new AudioContext();
-              runSpinSound(totalTime, totalRotations, context);
-              runSwooshSound(totalTime, totalRotations, context);
+              startSound();
+              // sourceSwoosh?.start();
+              // sourceMain?.start();
             }
           }}
         />
