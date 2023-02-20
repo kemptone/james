8597a998692d191f09e72aab3e1bf63634762d3b
justin kempton <justin@talentsky.com>
2023-02-19 17:18:20 -0800

    fix for localstorage issue

--- components/numberGame.actions.jsx
+++ components/numberGame.actions.jsx
@@ -1,78 +1,100 @@
 import { Button } from "./Button.tsx";
-import { Number1MultiplyBy, Number2MultiplyBy, CurrentMathProblem, Step } from '../data/State.ts'
-import Dialog from '../components/Dialog.tsx'
+import {
+  CurrentMathProblem,
+  Number1MultiplyBy,
+  Number2MultiplyBy,
+  Step,
+} from "../data/State.ts";
+import Dialog from "../components/Dialog.tsx";
 
 export default () => {
-
   const factorPlus = (val, Num) => () => {
-    Num.value += val
+    Num.value += val;
     // Step.value++
-  }
+  };
 
   const makeHarderBy = (by, Num) => () => {
-    Num.value *= by
+    Num.value *= by;
     // Step.value++
-  }
+  };
 
   return (
     <Dialog>
-      {D => (
+      {(D) => (
         <>
           <a onClick={D.openDialog}>settings</a>
 
           <D.Dialog ref={D.ref}>
-            <form onSubmit={e => {
-              e.preventDefault()
-            }}>
-          <dfl>
-            <dt>
-              SIZE A
-              [ <code children={ Number1MultiplyBy.value } /> ]
-            </dt>
-            <dd>
-              <button onClick={makeHarderBy(.1, Number1MultiplyBy)}>÷ 10</button>
-              <button onClick={makeHarderBy(10, Number1MultiplyBy)}>× 10</button>
-              <button onClick={factorPlus(-1, Number1MultiplyBy)}>- 1</button>
-              <button onClick={factorPlus(1, Number1MultiplyBy)}>+ 1</button>
-            </dd>
-            <dt>
-              SIZE B
-              [ <code children={ Number2MultiplyBy.value } /> ]
-            </dt>
-            <dd>
-              <button onClick={makeHarderBy(.1, Number2MultiplyBy)}>÷ 10</button>
-              <button onClick={makeHarderBy(10, Number2MultiplyBy)}>× 10</button>
-              <button onClick={factorPlus(-1, Number2MultiplyBy)}>- 1</button>
-              <button onClick={factorPlus(1, Number2MultiplyBy)}>+ 1</button>
-            </dd>
-            <dt>STEP</dt>
-            <dd>
-              <button onClick={e => Step.value++}>+ 1</button>
-            </dd>
-            <dt></dt>
-            <dd>
-              <button 
-                id="reset-button"
-                onClick={ e => {
-                  caches.delete("my-cache")
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <dfl>
+                <dt>
+                  SIZE A [ <code children={Number1MultiplyBy.value} /> ]
+                </dt>
+                <dd>
+                  <button onClick={makeHarderBy(.1, Number1MultiplyBy)}>
+                    ÷ 10
+                  </button>
+                  <button onClick={makeHarderBy(10, Number1MultiplyBy)}>
+                    × 10
+                  </button>
+                  <button onClick={factorPlus(-1, Number1MultiplyBy)}>
+                    - 1
+                  </button>
+                  <button onClick={factorPlus(1, Number1MultiplyBy)}>
+                    + 1
+                  </button>
+                </dd>
+                <dt>
+                  SIZE B [ <code children={Number2MultiplyBy.value} /> ]
+                </dt>
+                <dd>
+                  <button onClick={makeHarderBy(.1, Number2MultiplyBy)}>
+                    ÷ 10
+                  </button>
+                  <button onClick={makeHarderBy(10, Number2MultiplyBy)}>
+                    × 10
+                  </button>
+                  <button onClick={factorPlus(-1, Number2MultiplyBy)}>
+                    - 1
+                  </button>
+                  <button onClick={factorPlus(1, Number2MultiplyBy)}>
+                    + 1
+                  </button>
+                </dd>
+                <dt>STEP</dt>
+                <dd>
+                  <button onClick={(e) => Step.value++}>+ 1</button>
+                </dd>
+                <dt></dt>
+                <dd>
+                  <button
+                    id="reset-button"
+                    onClick={(e) => {
+                      localStorage.clear();
+                      caches.delete("my-cache");
 
-                  navigator.serviceWorker.getRegistration().then(registration => {
-                      registration?.unregister().then(() => {
-                        console.log('Service worker unregistered');
-                        location.reload()
-                      });
-                    });
-
-                }}
-              >Reinstall App</button>
-            </dd>
-          </dfl>
-
+                      navigator.serviceWorker.getRegistration().then(
+                        (registration) => {
+                          registration?.unregister().then(() => {
+                            console.log("Service worker unregistered");
+                            location.reload();
+                          });
+                        },
+                      );
+                    }}
+                  >
+                    Reinstall App
+                  </button>
+                </dd>
+              </dfl>
             </form>
           </D.Dialog>
         </>
       )}
     </Dialog>
-  )
-
-}
+  );
+};
