cd35a7d439e588ab35d135bdfeca3e8fb0af2a53
justin kempton <justin@talentsky.com>
2023-02-22 12:30:56 -0800

    now I have a list of audios

--- islands/Recorder.tsx
+++ islands/Recorder.tsx
@@ -4,6 +4,7 @@ const AudioRecorder = () => {
   const [stream, setStream] = useState<MediaStream | null>(null);
   const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
   const [audioURL, setAudioURL] = useState<string | null>(null);
+  const [audioURLs, setAudioURLS] = useState<string[]>([]);
   const [isRecording, setIsRecording] = useState(false);
 
   useEffect(() => {
@@ -16,29 +17,76 @@ const AudioRecorder = () => {
   }, []);
 
   useEffect(() => {
+    // opens the connection to the IndexDB database
     const dbRequest = window.indexedDB.open("audio", 1);
+
+    // deno-lint-ignore no-explicit-any
     dbRequest.onerror = (event: any) => {
-      console.error("Failed to open IndexedDB:", event.target.error);
+      console.error("Failed to open IndexedDB:", event.currentTarget.error);
     };
+
+    // deno-lint-ignore no-explicit-any
     dbRequest.onsuccess = (event: any) => {
-      const db = event.target.result;
+      // this is the reference to the object store
+      const db = event.currentTarget.result;
+      // tx = transaction
       const tx = db.transaction("recordings", "readonly");
+      // object store
       const store = tx.objectStore("recordings");
-      const index = store.index("timestamp");
-      const request = index.openCursor(null, "prev");
+
+      const request = store.openCursor();
+
+      const audios: string[] = [];
+      // deno-lint-ignore no-explicit-any
       request.onsuccess = (event: any) => {
-        const cursor = event.target.result;
+        const cursor: IDBCursorWithValue = event.target.result;
         if (cursor) {
-          setAudioURL(URL.createObjectURL(cursor.value.blob));
+          audios.push(URL.createObjectURL(cursor.value.blob));
+          cursor.continue();
+        } else {
+          if (audios.length) {
+            setAudioURLS(audios);
+          }
         }
       };
+
+      // deno-lint-ignore no-explicit-any
       tx.onerror = (event: any) => {
         console.error(
           "Failed to read recordings from IndexedDB:",
-          event.target.error,
+          event.currentTarget.error,
         );
       };
     };
+
+    // dbRequest.onsuccess = (event: any) => {
+    //   // this is the reference to the object store
+    //   const db = event.target.result;
+    //   // tx = transaction
+    //   const tx = db.transaction("recordings", "readonly");
+    //   // object store
+    //   const store = tx.objectStore("recordings");
+
+    //   // This creates an index based off of the timestamp itself
+    //   const index = store.index("timestamp");
+    //   // then it looks for that in the data store
+    //   const request = index.openCursor(null, "prev");
+
+    //   request.onsuccess = (event: any) => {
+    //     const cursor = event.target.result;
+    //     if (cursor) {
+    //       setAudioURL(URL.createObjectURL(cursor.value.blob));
+    //       cursor.continue();
+    //     }
+    //   };
+
+    //   tx.onerror = (event: any) => {
+    //     console.error(
+    //       "Failed to read recordings from IndexedDB:",
+    //       event.target.error,
+    //     );
+    //   };
+    // };
   }, []);
 
   const startRecording = () => {
@@ -53,35 +101,44 @@ const AudioRecorder = () => {
 
   const handleDataAvailable = (e: BlobEvent) => {
     const blob = e.data;
-    setAudioURL(URL.createObjectURL(blob));
+    // setAudioURL(URL.createObjectURL(blob));
     saveAudio(blob);
   };
 
   const saveAudio = (blob: Blob) => {
     const dbRequest = window.indexedDB.open("audio", 1);
+
     dbRequest.onerror = (event: any) => {
-      console.error("Failed to open IndexedDB:", event.target.error);
+      console.error("Failed to open IndexedDB:", event.currentTarget.error);
     };
+
     dbRequest.onupgradeneeded = (event: any) => {
-      const db = event.target.result;
+      const db = event.currentTarget.result;
       const store = db.createObjectStore("recordings", {
         keyPath: "id",
         autoIncrement: true,
       });
       store.createIndex("timestamp", "timestamp", { unique: false });
     };
+
     dbRequest.onsuccess = (event: any) => {
-      const db = event.target.result;
+      debugger;
+      const db = event.currentTarget.result;
       const tx = db.transaction("recordings", "readwrite");
       const store = tx.objectStore("recordings");
       const timestamp = Date.now();
       const data = { blob, timestamp };
+
       store.add(data);
+
       tx.oncomplete = () => {
         console.log("Audio saved to IndexedDB:", data);
       };
       tx.onerror = (event: any) => {
-        console.error("Failed to save audio to IndexedDB:", event.target.error);
+        console.error(
+          "Failed to save audio to IndexedDB:",
+          event.currentTarget.error,
+        );
       };
     };
   };
@@ -94,7 +151,14 @@ const AudioRecorder = () => {
       <button onClick={stopRecording} disabled={!isRecording}>
         Stop Recording
       </button>
-      {audioURL && <audio src={audioURL} controls />}
+      <div>
+        {audioURLs.map((audioURL, index) => (
+          <div>
+            <audio src={audioURL} controls />
+          </div>
+        ))}
+      </div>
+      {audioURL && <audio key={audioURL} src={audioURL} controls />}
     </div>
   );
 };
