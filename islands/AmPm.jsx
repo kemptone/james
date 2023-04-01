760a591adeb4d6f17b193326771848bd6eaa96fb
justin kempton <justin@talentsky.com>
2023-03-31 19:04:06 -0700

    adding main spinner

--- islands/AmPm.jsx
+++ islands/AmPm.jsx
@@ -2,5 +2,9 @@ import { useEffect, useState } from "preact/hooks";
 import Dialog from "../components/Dialog.tsx";
 
 export default () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>AM PM GAME</h1>
+    </div>
+  );
 };
