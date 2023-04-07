36ef16fb956bb662f0c294270190d1f1cf60e394
justin kempton <justin@talentsky.com>
2023-04-07 15:45:00 -0700

    ok, does this work?

--- islands/LiveCams.tsx
+++ islands/LiveCams.tsx
@@ -30,7 +30,7 @@ add("https://www.youtube.com/watch?v=1-iS7LArMPA");
 add("https://www.youtube.com/watch?v=pXe8MpU7uzk");
 add("https://www.youtube.com/watch?v=gcDWT-mTCOI");
 // add("https://www.youtube.com/watch?v=fFj4wnSTYtM");
-// add("https://www.youtube.com/watch?v=-HxJYZI9AMk");
+add("https://www.youtube.com/watch?v=-HxJYZI9AMk");
 add("https://www.youtube.com/watch?v=1lPnLBUIlko");
 
 export default () => {
